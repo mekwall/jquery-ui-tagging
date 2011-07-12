f6d89bd023e2c242fe12157c85e95e201bfdb16f
Daniel Gatis Carrazzoni <danielgatis@gmail.com>
2011-07-12 03:30:24 -0300

    fixing resize and autocomplete

--- src/jquery.ui.tagging.js
+++ src/jquery.ui.tagging.js
@@ -1,51 +1,99 @@
 (function($){
-
     $.widget("ui.tagging", {
-        // default options
         options: {
             source: [],
             maxItemDisplay: 3,
-            autosize: true,
-            animateResize: false,
-            animateDuration: 50
+            token: "@",
+            onResize : function(){},
+            animate : true,
+            animateDuration : 150,
+            animateCallback : function(){},
+            extraSpace : 20,
+            limit: 1000
         },
-        _create: function() {
+        props:"vertical-align" +			                     
+	          " direction" +
+	          " font-size-adjust" +
+	    	  " font-size" +
+	    	  " font-stretch" +
+			  " font-style" +
+			  " font-family" +
+			  " font-variant" +
+			  " font-weight" +
+			  " letter-spacing" +
+			  " word-spacing" +
+			  " line-height" +
+			  " text-align" +
+			  " text-transform" +
+			  " text-decoration",
+        _createAutoResize: function() {
+        	var self = this;
+        	var textarea = this.element.css({resize:'none','overflow-y':'hidden'}),
+                origHeight = textarea.height(),                
+                clone = (function(){
+                	 var propOb = {};
+                    $.each((self.props + "height width").split(" "), function(i, prop){
+                        propOb[prop] = textarea.css(prop);
+                    });
+                    return textarea.clone().removeAttr('id').removeAttr('name').css({
+                        position: 'absolute',
+                        top: 0,
+                        left: -9999
+                    }).css(propOb).attr('tabIndex','-1').insertBefore(textarea);
+                })(),
+                lastScrollTop = null,
+                updateSize = function() {
+					clone.height(0).val(self.element.val()).scrollTop(10000);
+                    var scrollTop = Math.max(clone.scrollTop(), origHeight) + self.options.extraSpace,
+                        toChange = self.element.add(clone);	
+                    if (lastScrollTop === scrollTop) { return; }
+                    lastScrollTop = scrollTop;
+					if ( scrollTop >= self.options.limit ) {
+                        self.elemnt.css('overflow-y','');
+                        return;
+                    }
+                    self.options.onResize.call(self.element);
+					self.options.animate && textarea.css('display') === 'block' ?
+                        toChange.stop().animate({height:scrollTop}, self.options.animateDuration, self.options.animateCallback)
+                        : toChange.height(scrollTop);
+                };
+            textarea
+                .bind('keyup', updateSize)
+                .bind('keydown', updateSize)
+                .bind('change', updateSize);
+            
+	        },
+        _createAutoComplete: function() {
             var self = this;
             
+            this.marks = {};
             this.activeSearch = false;
             this.searchTerm = "";
             this.beginFrom = 0;
-
+            
             this.wrapper = $("<div>")
                 .addClass("ui-tagging-wrap");
             
-            this.highlight = $("<span></span>")
-            	.css({
-            		"direction" : this.element.css("direction"),
-					"font-size-adjust" : this.element.css("font-size-adjust"),
-					"font-size" : this.element.css("font-size"),
-					"font-stretch" : this.element.css("font-stretch"),
-					"font-style" : this.element.css("font-style"),
-					"font-family" : this.element.css("font-family"),
-					"font-variant" : this.element.css("font-variant"),
-					"font-weight" : this.element.css("font-weight"),
-					"letter-spacing" : this.element.css("letter-spacing"),
-					"line-height" :  this.element.css("line-height")
-	        	});
+            this.highlight = $("<span></span>");
+            var css = {};
+            $.each(self.props.split(" "), function(i, prop){
+            	css[prop] = self.element.css(prop);
+            });
+            this.highlight.css(css);
             
-            this.highlightWrapper = $("<mark></mark>")
+	        this.highlightWrapper = $("<mark></mark>")
             	.addClass("ui-corner-all");
-			
+            
             this.highlightContainer = $("<div>")
-            	.css({"padding" : '6px'})
-        		.width(this.element.width() - 12)
+            	.css({paddingLeft: '1px'})
+            	.width(this.element.width()-1)
                 .addClass("ui-tagging-highlight")
                 .append(this.highlight);
-			
+            
             this.meta = $("<input>")
                 .attr("type", "hidden")
                 .addClass("ui-tagging-meta");
-
+            
             this.container = $("<div></div>")
                 .width(this.element.width())
                 .insertBefore(this.element)
@@ -55,84 +103,43 @@
                     this.element.wrap(this.wrapper).parent(),
                     this.meta
                 );
-            
-            var initialHeight = this.element.height();
-            
-            this.element.height(this.element.css('lineHeight'));
-            
+                
             this.element.keypress(function(e) {
-                // activate on @
-                if (e.which == 64 && !self.activeSearch) {
+                // activate on token
+                if (e.which == self.options.token.charCodeAt() && !self.activeSearch) {
                     self.activeSearch = true;
                     self.beginFrom = e.target.selectionStart + 1;
                 }
-                // deactivate on space
-                if (e.which == 32 && self.activeSearch) {
-                    self.activeSearch = false;
+                // deactivate on space(32) or escape(0 or 27)
+                if ((e.which == 32 || e.which == 27 || e.which == 0) && self.activeSearch) {
+                	self.activeSearch = false;
+                    self.element.autocomplete("close");
                 }
-            }).bind("expand keyup change", function(e) {
-                var cur = self.highlight.find("mark"),
-                    val = self.element.val(),
-                    prevHeight = self.element.height(),
-                    rowHeight = self.element.css('lineHeight'),
-                    newHeight = 0;
-            	marks = {}    
-                cur.each(function(i) {
-                    var s = $(this);
-                    if (!(s.text() in marks)) {
-                    	val = val.replace(new RegExp(s.text(), 'g'), $("<div>").append(s).html());
-                    	marks[s.text()] = 1;
-                	}
-                });
-                self.highlight.html(val);
-                newHeight = self.element.height(rowHeight)[0].scrollHeight;
-                self.element.height(prevHeight);
-                if (newHeight < initialHeight) {
-                    newHeight = initialHeight;
+            });
+            
+            this.element.bind("expand keydown keyup change", function(e) {
+                var val = self.element.val();
+                for (mark in self.marks) {
+                	val = val.replace(new RegExp(mark, 'g'), self.marks[mark]);
                 }
-                if (!$.browser.mozilla) {
-                    if (self.element.css('paddingBottom') || self.element.css('paddingTop')) {
-                        var padInt =
-                            parseInt(self.element.css('paddingBottom').replace('px', '')) +
-                            parseInt(self.element.css('paddingTop').replace('px', ''));
-                        newHeight -= padInt;
-                    }
-                }
-                self.options.animateResize ?
-                    self.element.stop(true, true).animate({
-                            height: newHeight
-                        }, self.options.animateDuration) :
-                    self.element.height(newHeight);
-                
-                var widget = self.element.autocomplete("widget");
-                    widget.position({
-                        my: "left top",
-                        at: "left bottom",
-                        of: self.container
-                    }).width(self.container.width()-4);
-                                        
-            }).autocomplete({
+                self.highlight.html(val);                                      
+            });
+            
+            this.element.autocomplete({
+            	autoFocus: true,
                 minLength: 0,
                 delay: 0,
                 maxDisplay: this.options.maxItemDisplay,
-                open: function(event, ui) {
-                    var widget = $(this).autocomplete("widget");
-                    widget.position({
-                        my: "left top",
-                        at: "left bottom",
-                        of: self.container
-                    }).width(self.container.width()-4);
-                },
+                position : { my: "left top", at: "left bottom", of: self.container },
                 source: function(request, response) {
                     if (self.activeSearch) {
                         self.searchTerm = request.term.substring(self.beginFrom);
-                        if (request.term.substring(self.beginFrom - 1, self.beginFrom) != "@") {
+                        if (request.term.substring(self.beginFrom - 1, self.beginFrom) != self.options.token) {
                             self.activeSearch = false;
                             self.beginFrom = 0;
                             self.searchTerm = "";
                         }
                         if (self.searchTerm != "") {
-                            
                             if ($.type(self.options.source) == "function") {
                                 self.options.source(request, response);                   
                             } else {
@@ -149,27 +156,28 @@
                     }
                 },
                 focus: function() {
-                    // prevent value inserted on focus
                     return false;
                 },
                 select: function(event, ui) {
                     self.activeSearch = false;
-                    this.value = this.value.replace("@" + self.searchTerm, ui.item.label) + ' ';
+                    this.value = this.value.replace(self.options.token + self.searchTerm, ui.item.label) + ' ';
+                    self.marks[ui.item.label] =  $("<div>").append(self.highlightWrapper
+                    												   .text(ui.item.label)
+                                                                       .clone()).html();
                     self.highlight.html(
                         self.highlight.html()
-                            .replace("@" + self.searchTerm,
-                                     $("<div>").append(
-                                         self.highlightWrapper
-                                             .text(ui.item.label)
-                                             .clone()
-                                     ).html()+' ')
+                            .replace(self.options.token + self.searchTerm, self.marks[ui.item.label] + ' ' )
                     );     
-                    self.meta.val((self.meta.val() + " @[" + ui.item.value + ":" + ui.item.label +"]").trim());
+                    self.meta.val((self.meta.val() + " " + self.options.token + "[" + ui.item.value + ":" + ui.item.label +"]").trim());
                     return false;
                 }
             });
-
+        },
+        _create: function() {
+        	if (this.element[0].nodeName.toLowerCase() != "textarea" && this.element.data('ui.tagging') != undefined) return;
+        	this.element.data('ui.tagging', true);
+        	this._createAutoComplete();
+        	this._createAutoResize();
         }
     });
-
 })(jQuery);
